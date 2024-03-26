a7954e80aa263e52472a9bfd316913ad4bbb6775
Ali070 <Ali.zidan39@yahoo.com>
2024-03-26 20:45:09 +0200

    + Finish handling apis using MVVM provider

--- lib/ui/home/widgets/categoroy_details.dart
+++ lib/ui/home/widgets/categoroy_details.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:news_c10_maadi/shared/api/api_manager.dart';
+import 'package:news_c10_maadi/ui/home/viewModels/sources_view_model.dart';
 import 'package:news_c10_maadi/ui/home/widgets/news_list_widget.dart';
 import 'package:news_c10_maadi/ui/home/widgets/source_widget.dart';
+import 'package:provider/provider.dart';
 
 import '../../../model/category_model.dart';
 
@@ -15,10 +17,63 @@ class CategoryDetails extends StatefulWidget {
 
 class _CategoryDetailsState extends State<CategoryDetails> {
   int selectedSource = 0;
-
+  var viewModel = SourcesViewModel();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    viewModel.getSources(widget.categoryModel.id);
+  }
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
+    return ChangeNotifierProvider(
+        create:(_)=>viewModel,
+        child: Consumer<SourcesViewModel>(
+            builder:(context , viewmodel,_){
+              if(viewModel.isLoading){
+                return Center(child: CircularProgressIndicator(),);
+              }else if(viewModel.errorMessage != null){
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(viewModel.errorMessage??""),
+                    ElevatedButton(onPressed: (){
+                      viewModel.getSources(widget.categoryModel.id);
+                    }, child: Text("Try again"))
+                  ],
+                );
+              }
+              var sources = viewModel.sourcesList;
+              return DefaultTabController(
+                length: sources.length,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      TabBar(
+                          onTap: (index){
+                            selectedSource = index;
+                            setState(() {
+
+                            });
+                          },
+                          isScrollable: true,
+                          indicatorColor: Colors.transparent,
+                          dividerColor: Colors.transparent,
+                          tabs: sources.map((source) => SourceWidget(
+                            source: source,
+                            isSelected: selectedSource == sources.indexOf(source),
+                          )).toList()
+                      ),
+                      NewsListWidget(source: sources[selectedSource],)
+                    ],
+                  ),
+                ),
+              );
+
+            }
+        ),
+    );/*FutureBuilder(
         future: ApiManager.getSources(widget.categoryModel.id),
         builder: (context , snapshot){
           if(snapshot.connectionState == ConnectionState.waiting){
@@ -64,6 +119,6 @@ class _CategoryDetailsState extends State<CategoryDetails> {
             ),
           );
         }
-    );
+    )*/;
   }
 }
